4d5fab158af8a3711eee659ad7fd113a18536056
arnauld.alex <arnauld.alex@soprasteria.com>
2018-10-09 16:44:16 +0200

    first fix Module 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from "./app.component";
-import { ProductPageComponent } from "./product-page/product-page.component";
-import { ProductDescriptionComponent } from "./product-description/product-description.component";
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
-import { ProductService } from "./product.service";
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
